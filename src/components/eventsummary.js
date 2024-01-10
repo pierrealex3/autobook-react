95a0d7a5bcc88e600a2179f739e70cea7bb9ddbf
PA Lemire <pierrealex3@hotmail.com>
2024-01-09 22:52:33 -0500

    Remove dist folder from Git

--- src/components/eventsummary.js
+++ src/components/eventsummary.js
@@ -1,9 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function EventSummary() {
 
     const createVehicleNavHook = useNavigate();
-    
+    const [vehicles, setVehicles] = useState([]);
+
     function createVehicule() {
         createVehicleNavHook("/vehicle");
 
@@ -13,7 +15,7 @@ export default function EventSummary() {
         <div>
             <h4>Logged on user: {localStorage.getItem("user")}</h4>
             <div>
-                <input type="button" value="New Vehicle" className="btn btn-primary" onClick={createVehicule} />
+                { vehicles.length == 0 ? <input type="button" value="New Vehicle" className="btn btn-primary" onClick={createVehicule} /> : <h4>VEHICLE SHIFTER</h4> }
             </div>
         </div>
     )
