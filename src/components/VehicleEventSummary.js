90aa00ab5b0d26e91b1bcaeec4c2f6aefc8086c4
PA Lemire <pierrealex3@hotmail.com>
2024-02-06 21:58:57 -0500

    Basic styling for event summary page

--- src/components/VehicleEventSummary.js
+++ src/components/VehicleEventSummary.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import useFetch from "../services/FetchForGetHook";
+import { vehicleBrandImageMap } from "../utils/ImageUtils";
 
 export default function VehicleEventSummary() {
 
@@ -8,6 +9,7 @@ export default function VehicleEventSummary() {
     const { dataArr:vehicles, error:errorVehicles, loading:loadingVehicles } = useFetch("vehicles");
     const { dataArr:events, error:errorEvents, loading:loadingEvents } = useFetch("events");
     const [selectedVehicle, setSelectedVehicle] = useState(null);
+    let vehicleIdPropsMap = {}; /* vehicle id as top-level key */
 
     function createVehicule() {
         navHook("/vehicle");
@@ -25,6 +27,15 @@ export default function VehicleEventSummary() {
 
     if (loadingVehicles || loadingEvents) return (<div>SPINNER</div>);
 
+
+    vehicles.forEach( v => {
+        vehicleIdPropsMap[v.id] = {
+        brand: v.brand,
+        model: v.model,
+        year: v.year
+     }
+    });
+
     return (
         <div>
             <h4>Logged on user: {localStorage.getItem("user")}</h4>
@@ -32,7 +43,11 @@ export default function VehicleEventSummary() {
                 {
                     vehicles.length == 0 ? <input type="button" value="New Vehicle" className="btn btn-primary" onClick={createVehicule} /> :
                         vehicles.map(
-                            v => <div key={v.id} style={{border: 'solid', backgroundColor: v === selectedVehicle ? 'lime' : 'white'}} onClick={ () => handleOnVehicleClick(v) }><h4>{v.brand} {v.model} {v.year}</h4></div>
+                            v => 
+                            <div key={v.id} style={{border: 'outset', backgroundColor: v === selectedVehicle ? 'lime' : 'white'}} onClick={ () => handleOnVehicleClick(v) }>
+                            <img src={vehicleBrandImageMap[v.brand]} alt={v.brand} width="100" />
+                            <h4>{v.brand} {v.model} {v.year}</h4>
+                            </div>
                             )
                 }
             </div>
@@ -44,7 +59,8 @@ export default function VehicleEventSummary() {
             <div>
               {events.map(
                 eventx => 
-                <div key={eventx.id} style={{ border: 'dotted', backgroundColor: 'orange'}}>
+                <div key={eventx.id} style={{ border: 'groove', backgroundColor: 'lightgray'}}>
+                  <img src={vehicleBrandImageMap[vehicleIdPropsMap[eventx.vehicleId].brand]} alt={vehicleIdPropsMap[eventx.vehicleId].brand} width="75" />
                   <h4>{eventx.vehicleTag}</h4>
                   <p>
                   {eventx.date}
---
+++ src/utils/ImageUtils.js
@@ -0,0 +1,5 @@
+
+export const vehicleBrandImageMap = {
+  Audi : "/images/png-transparent-audi-logo.png",
+  BMW: "/images/png-transparent-bmw-logo.png"
+};
